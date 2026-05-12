d366f20a17a54a27f179f3f1f8b626cca8819ad4
grimza99 <gbtmxlf@naver.com>
2026-05-12 11:36:55 +0900

    feat(shared): add toast message constants

--- package-shared/src/constants/index.ts
+++ package-shared/src/constants/index.ts
@@ -2,3 +2,4 @@ export { API_PATHS } from "./api";
 export { APP_DESCRIPTION, APP_NAME, SESSION_STORAGE_KEY } from "./app";
 export { BUCKET_NAME } from "./supabase";
 export { bikerMapTheme, colorTokens, shadowTokens } from "./theme";
+export { TOAST_MESSAGE } from "./toast";
