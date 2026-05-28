0ea87340495097686aae113020806e4ead614fa8
grimza99 <gbtmxlf@naver.com>
2026-05-28 14:47:06 +0900

    feat : form area 에 gap 추가

--- mobile/features/auth/LogInForm.tsx
+++ mobile/features/auth/LogInForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
 
 import { LoginBody } from "@package-shared/index";
 
@@ -15,24 +15,26 @@ export default function LogInForm({ onSubmit }: ILogInForm) {
   const [password, setPassword] = useState("");
 
   return (
-    <ScrollView style={[containerStyles.panel, formStyles.formContainer]}>
-      <Input
-        label="이메일"
-        onChange={(e) => {
-          setEmail(e.nativeEvent.text);
-        }}
-        style={{ gap: 10 }}
-        fieldStyle={formStyles.fieldStyle}
-      />
-      <Input
-        label="비밀번호"
-        onChange={(e) => {
-          setPassword(e.nativeEvent.text);
-        }}
-        style={{ gap: 10 }}
-        fieldStyle={formStyles.fieldStyle}
-      />
-      <Button onPress={() => onSubmit({ email, password })}>로그인</Button>
+    <ScrollView style={[containerStyles.panel]}>
+      <View style={[formStyles.formContainer]}>
+        <Input
+          label="이메일"
+          onChange={(e) => {
+            setEmail(e.nativeEvent.text);
+          }}
+          style={{ gap: 10 }}
+          fieldStyle={formStyles.fieldStyle}
+        />
+        <Input
+          label="비밀번호"
+          onChange={(e) => {
+            setPassword(e.nativeEvent.text);
+          }}
+          style={{ gap: 10 }}
+          fieldStyle={formStyles.fieldStyle}
+        />
+        <Button onPress={() => onSubmit({ email, password })}>로그인</Button>
+      </View>
     </ScrollView>
   );
 }
--- mobile/features/auth/SignUpForm.tsx
+++ mobile/features/auth/SignUpForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
 
 import { SignUpBody } from "@package-shared/index";
 
@@ -16,34 +16,36 @@ export default function SignUpForm({ onSubmit }: ISignUpForm) {
   const [password, setPassword] = useState("");
 
   return (
-    <ScrollView style={[containerStyles.panel, formStyles.formContainer]}>
-      <Input
-        label="이름"
-        onChange={(e) => {
-          setName(e.nativeEvent.text);
-        }}
-        style={{ gap: 10 }}
-        fieldStyle={formStyles.fieldStyle}
-      />
-      <Input
-        label="이메일"
-        onChange={(e) => {
-          setEmail(e.nativeEvent.text);
-        }}
-        style={{ gap: 10 }}
-        fieldStyle={formStyles.fieldStyle}
-      />
-      <Input
-        label="비밀번호"
-        onChange={(e) => {
-          setPassword(e.nativeEvent.text);
-        }}
-        style={{ gap: 10 }}
-        fieldStyle={formStyles.fieldStyle}
-      />
-      <Button onPress={() => onSubmit({ name, email, password })}>
-        회원가입
-      </Button>
+    <ScrollView style={[containerStyles.panel]}>
+      <View style={[formStyles.formContainer]}>
+        <Input
+          label="이름"
+          onChange={(e) => {
+            setName(e.nativeEvent.text);
+          }}
+          style={{ gap: 10 }}
+          fieldStyle={formStyles.fieldStyle}
+        />
+        <Input
+          label="이메일"
+          onChange={(e) => {
+            setEmail(e.nativeEvent.text);
+          }}
+          style={{ gap: 10 }}
+          fieldStyle={formStyles.fieldStyle}
+        />
+        <Input
+          label="비밀번호"
+          onChange={(e) => {
+            setPassword(e.nativeEvent.text);
+          }}
+          style={{ gap: 10 }}
+          fieldStyle={formStyles.fieldStyle}
+        />
+        <Button onPress={() => onSubmit({ name, email, password })}>
+          회원가입
+        </Button>
+      </View>
     </ScrollView>
   );
 }
