35a8fd9d981c70366f6457173b17b38f524b9192
grimza99 <gbtmxlf@naver.com>
2026-05-29 01:26:25 +0900

    fix(mobile): stabilize expo asset loading setup

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -10,7 +10,4 @@ const config = getDefaultConfig(projectRoot);
 // package-shared 소스 변경을 감지하기 위해 해당 디렉토리만 watch
 config.watchFolders = [path.resolve(projectRoot, "../package-shared")];
 
-// node_modules는 mobile 로컬에서만 탐색 (root hoisting 방지)
-config.resolver.nodeModulesPaths = [path.resolve(projectRoot, "node_modules")];
-
 module.exports = config;
