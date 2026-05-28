6fc6a49accebfde7fbe1283fe145b089c2829b5a
grimza99 <gbtmxlf@naver.com>
2026-05-28 12:11:36 +0900

    chore: button 컴포넌트 데모 삭제

--- mobile/app/(tabs)/bikers.tsx
+++ mobile/app/(tabs)/bikers.tsx
@@ -7,10 +7,6 @@ export default function BikersScreen() {
       eyebrow="Expo Router shell"
       title="Biker Map"
       description="바이커 스크린"
-    >
-      <Button>테스트</Button>
-      <Button selected>선택 상태</Button>
-      <Button loading>로딩 상태</Button>
-    </AppScreen>
+    ></AppScreen>
   );
 }
