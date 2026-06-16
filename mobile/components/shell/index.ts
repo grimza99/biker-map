ac44e6ae060f594995ff5e5933231e31febdd0a0
grimza99 <gbtmxlf@naver.com>
2026-06-16 11:08:41 +0900

    chore : AuthRequiredPanel.tsx 삭제

--- mobile/components/shell/index.ts
+++ mobile/components/shell/index.ts
@@ -1,4 +1,3 @@
 export { AppScreen } from "./AppScreen";
-export { AuthRequiredPanel } from "./AuthRequiredPanel";
 export { MobileTabBar } from "./MobileTabBar";
 export { FloatingMapSheet } from "./floating-sheet/FloatingMapSheet";
