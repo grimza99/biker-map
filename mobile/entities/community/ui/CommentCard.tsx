26877ae10847f0fbf9cc1dc1113dc7c52b99f38b
grimza99 <gbtmxlf@naver.com>
2026-06-16 16:48:07 +0900

    feat :create Comment Card.tsx

---
+++ mobile/entities/community/ui/CommentCard.tsx
@@ -0,0 +1,39 @@
+import { View } from "react-native";
+import type { CommunityComment, CommunityReply } from "@package-shared/index";
+
+import { AppText, DefaultCardContainer } from "@/components/common";
+import { CommentActionBar } from "./CommentActionBar";
+import { ProfileIdentity } from "@/shared";
+
+type CommentCardProps = {
+  item: CommunityComment | CommunityReply;
+  disabled?: boolean;
+  showReplyAction?: boolean;
+  onReplyPress?: () => void;
+};
+
+//todo : author의 avatarUrl 넘겨주기
+export function CommentCard({
+  item,
+  disabled,
+  showReplyAction,
+  onReplyPress,
+}: CommentCardProps) {
+  return (
+    <DefaultCardContainer containerStyle="gap-2 rounded-3xl bg-panel-solid p-3">
+      <View className="flex-1 gap-1 flex-row items-center justify-between">
+        <ProfileIdentity avatarUrl={null} name={item.author.name} />
+        <AppText tone="muted" className="pl-10 text-xs font-semibold">
+          {item.timeLabel}
+        </AppText>
+      </View>
+      <AppText className="leading-6.5">{item.content}</AppText>
+      <CommentActionBar
+        item={item}
+        disabled={disabled}
+        showReplyAction={showReplyAction}
+        onReplyPress={onReplyPress}
+      />
+    </DefaultCardContainer>
+  );
+}
