cd64a93252fad40b54ba30807529427baa0c5ee9
grimza99 <gbtmxlf@naver.com>
2026-06-23 11:24:00 +0900

    refactor : post list api tanstack query로 변경

--- mobile/entities/community/model/index.ts
+++ mobile/entities/community/model/index.ts
@@ -1,2 +1,1 @@
-export * from "./community-post-api";
 export * from "./use-post-query";
--- mobile/entities/community/model/use-post-query.ts
+++ mobile/entities/community/model/use-post-query.ts
@@ -1,13 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+
 import {
   API_PATHS,
   type ApiResponse,
   PostCommentsResponseData,
   queryKeys,
   type PostDetailResponseData,
+  CommunityPostsQuery,
+  buildPostsQuery,
+  PostsListResponseData,
 } from "@package-shared/index";
-import { useQuery } from "@tanstack/react-query";
 
 import { apiFetch } from "@/shared";
+
+/**----------------------------------- post list --------------------------------- */
+
+export function usePostList(query: CommunityPostsQuery = {}) {
+  const postQuery = buildPostsQuery(query);
+  const endpoint = postQuery
+    ? `${API_PATHS.community.posts}?${postQuery}`
+    : API_PATHS.community.posts;
+
+  return useQuery<ApiResponse<PostsListResponseData>>({
+    queryKey: queryKeys.posts(query),
+    queryFn: async () => {
+      const res = await apiFetch.get<PostsListResponseData>(endpoint);
+      return res;
+    },
+  });
+}
 
 /**----------------------------------- post detail --------------------------------- */
 export function usePostDetail(postId: string) {
