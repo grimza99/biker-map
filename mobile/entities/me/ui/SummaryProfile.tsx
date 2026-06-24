34112cf3aa75acfd9bab6973a8f39e963ca225c6
grimza99 <gbtmxlf@naver.com>
2026-06-24 12:15:36 +0900

    feat : SummaryProfile 스켈레톤 적용

--- mobile/entities/me/ui/SummaryProfile.tsx
+++ mobile/entities/me/ui/SummaryProfile.tsx
@@ -4,32 +4,40 @@ import { AppText } from "@/components/common";
 import { cn } from "@/shared";
 import { useMyFavorites, useMyPosts } from "../model";
 import { Profile } from "@/shared/ui";
+import { SummaryProfileSkeleton } from "./SummaryProfileSkeleton";
 
 export function SummaryProfile() {
-  const { data: favoritePosts } = useMyFavorites(
-    { page: 0, pageSize: 0 },
-    "post"
-  );
-  const { data: favoriteRoutes } = useMyFavorites(
-    { page: 0, pageSize: 0 },
-    "route"
-  );
-  const { data: myPosts } = useMyPosts({ page: 1, pageSize: 1 });
+  const { data: favoritePosts, isLoading: favoritePostLoading } =
+    useMyFavorites({ page: 0, pageSize: 0 }, "post");
+  const { data: favoriteRoutes, isLoading: favoriteRouteLoading } =
+    useMyFavorites({ page: 0, pageSize: 0 }, "route");
+  const { data: myPosts, isLoading: myPostLoading } = useMyPosts({
+    page: 1,
+    pageSize: 1,
+  });
 
   const totalFavoriteCount =
     (favoritePosts?.meta?.total || 0) + (favoriteRoutes?.meta?.total || 0);
   const myPostsCount = myPosts?.meta?.total;
 
+  const isLoading =
+    favoritePostLoading || favoriteRouteLoading || myPostLoading;
   return (
-    <View className="flex-col gap-5">
-      <Profile />
-      <View className="border-2 border-border flex-row rounded-[18px] py-3 px-3 justify-around bg-panel-solid ">
-        <SummaryItem label="작성글" value={myPostsCount} />
-        <SummaryItem label="즐겨찾기" value={totalFavoriteCount} />
-        <SummaryItem label="받은 좋아요" value={null} />
-        {/* todo: 받은 좋아요 조회하는 기능 구현 */}
-      </View>
-    </View>
+    <>
+      {isLoading ? (
+        <SummaryProfileSkeleton />
+      ) : (
+        <View className="flex-col gap-5">
+          <Profile />
+          <View className="border-2 border-border flex-row rounded-[18px] py-3 px-3 justify-around bg-panel-solid ">
+            <SummaryItem label="작성글" value={myPostsCount} />
+            <SummaryItem label="즐겨찾기" value={totalFavoriteCount} />
+            <SummaryItem label="받은 좋아요" value={null} />
+            {/* todo: 받은 좋아요 조회하는 기능 구현 */}
+          </View>
+        </View>
+      )}
+    </>
   );
 }
 
